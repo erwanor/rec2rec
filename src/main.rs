e5cc4c46962431e4d78303f9fb1e5b0063f8af09
Jonathan Strickland <djanatyn@gmail.com>
2022-05-24 16:09:24 -0400

    feat: attempt to connect to list of harcoded peers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
+use clap::Parser;
+use std::io;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
-use tokio::{net::TcpListener, task::JoinError};
+use tokio::{
+    net::{TcpListener, TcpStream},
+    task::JoinError,
+};
 
 enum Status {
     Up,
@@ -13,22 +18,49 @@ struct Node {
     address: Ipv4Addr,
 }
 
+#[derive(Parser, Debug)]
+#[clap()]
+struct Args {
+    bind_address: String,
+}
+
+// TODO: parse the bind_address
+fn parse_bind_address(input: String) -> io::Result<SocketAddrV4> {
+    todo!();
+}
+
 async fn work(s: SocketAddr) {
     println!("doing work! with {}", s);
 }
 
 #[tokio::main]
 async fn main() -> Result<(), JoinError> {
+    let args: Args = Args::parse();
+    dbg!(args);
+
     // this is a list of peers to connect to
     // in the future, we would like to build this list dynamically
     // for now, we are starting our implementation by enumerating peers
-    let _peers: Vec<SocketAddrV4> = vec![
+    let peers: Vec<SocketAddrV4> = vec![
         SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080),
         SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8081),
         SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8082),
         SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8083),
         SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8084),
     ];
+
+    // attempt to connect to all peers
+    for peer in peers {
+        let stream = TcpStream::connect(&peer);
+
+        match stream.await {
+            Ok(_stream) => continue,
+            Err(e) => {
+                eprintln!("unable to connect to {peer}: {e}");
+                continue;
+            }
+        }
+    }
 
     println!("starting rec2rec!!!");
     // config
@@ -41,4 +73,8 @@ async fn main() -> Result<(), JoinError> {
         }
     })
     .await
+
+    // currently our tokio task runs forever
+    // when we connect to listeners, we want to also continue listening
+    // we need to run both client connections and the listener at the same time
 }
