bf2c04a1a7a5bea596e1033442f2d8e864f91141
Jonathan Strickland <djanatyn@gmail.com>
2022-05-25 12:59:34 -0400

    feat: client talks server
    this assumes that we're listening on 127.0.0.1:8080
    
    we saw some issues with tokio::io::AsyncReadExt#read_to_string where it
    wasn't completing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use clap::Parser;
 use std::io;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
     task::JoinError,
 };
@@ -19,24 +20,90 @@ struct Node {
 }
 
 #[derive(Parser, Debug)]
-#[clap()]
 struct Args {
     bind_address: String,
 }
 
-async fn work(s: SocketAddr) {
-    println!("doing work! with {}", s);
+#[derive(Debug)]
+enum Messages {
+    Ping,
+    Pong,
+}
+
+/// This handles incoming connections to our listener
+///
+/// Currently, it listens for "ping" and says "pong".
+async fn work(mut stream: TcpStream, s: SocketAddr) {
+    println!("got connection from {s}!");
+
+    println!("server reading string");
+    let mut buffer = [0; 10];
+    let _characters_recieved = stream
+        .read(&mut buffer)
+        .await
+        .expect("could not read message");
+
+    // TODO: it seems like we never reach this point
+    println!("done reading string");
+
+    dbg!(buffer);
+
+    // if buffer != b"ping" {
+    //     panic!("did not get ping: {buffer}");
+    // }
+
+    stream
+        .write_all(b"pong\n")
+        .await
+        .expect("failed to write to client stream");
+}
+
+/// This handles incoming connections to our listener
+///
+/// Currently, it says "ping".
+async fn client_work(mut stream: TcpStream) {
+    stream
+        .write_all(b"ping\n")
+        .await
+        .expect("failed to write to stream");
+
+    println!("i sent a ping to {stream:?}");
+
+    // should we wait a bit after sending the ping?
+    // better, can we wait until we recieve data?
+    let mut buffer = String::new();
+    let characters_recieved = stream
+        .read_to_string(&mut buffer)
+        .await
+        .expect("could not read message");
+
+    dbg!(&buffer);
+    dbg!(characters_recieved);
+
+    if buffer != "pong" {
+        panic!("did not get pong: {buffer}");
+    }
 }
 
 #[tokio::main]
-async fn main() -> Result<(), JoinError> {
+async fn main() {
     let args: Args = Args::parse();
+
+    let bind_address = args.bind_address;
+    let listener = tokio::spawn(async move {
+        let listener = TcpListener::bind(&bind_address).await.unwrap();
+        println!("started listening on {bind_address}");
+        loop {
+            let (stream, addr) = listener.accept().await.unwrap();
+            tokio::spawn(work(stream, addr));
+        }
+    });
 
     // this is a list of peers to connect to
     // in the future, we would like to build this list dynamically
     // for now, we are starting our implementation by enumerating peers
     let peers: Vec<SocketAddrV4> = vec![
-        SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080),
+        SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080), // ourself!
         SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8081),
         SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8082),
         SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8083),
@@ -48,7 +115,11 @@ async fn main() -> Result<(), JoinError> {
         let stream = TcpStream::connect(&peer);
 
         match stream.await {
-            Ok(_stream) => continue,
+            // this is sending outgoing client connections
+            Ok(stream) => {
+                tokio::spawn(client_work(stream));
+                continue;
+            }
             Err(e) => {
                 eprintln!("unable to connect to {peer}: {e}");
                 continue;
@@ -57,17 +128,12 @@ async fn main() -> Result<(), JoinError> {
     }
 
     println!("starting rec2rec!!!");
+
     // config
-    let bind_address = args.bind_address;
-    tokio::spawn(async move {
-        let listener = TcpListener::bind(&bind_address).await.unwrap();
-        println!("started listening on {bind_address}");
-        loop {
-            let (_, addr) = listener.accept().await.unwrap();
-            tokio::spawn(work(addr));
-        }
-    })
-    .await
+
+    // once we have additional tasks, we would await them here as well
+    // we may need to join all of the futures together
+    let _ = listener.await;
 
     // currently our tokio task runs forever
     // when we connect to listeners, we want to also continue listening
