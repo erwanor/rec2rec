dc005e9686ccff1027e815b16a85fc3180800c8e
aaronwinter <erwan.ounn.84@gmail.com>
2022-07-26 01:00:32 +0300

    feat: use codec adapters for IO (wip)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+#![feature(inherent_associated_types)]
 #![allow(dead_code)]
 #![allow(unused_imports)]
+use anyhow::Result;
 use bytes::{Buf, BufMut, BytesMut};
 use clap::Parser;
 use core::fmt;
@@ -14,150 +16,41 @@ use tokio::{
     net::{TcpListener, TcpStream},
     task::JoinError,
 };
+mod connection;
+mod message;
+mod peer;
+mod state;
+use crate::message::Message;
+use crate::peer::Peer;
+use thiserror::Error;
 
-#[derive(Debug)]
-enum State {
-    Offline,
-    Syncing,
-    Connected,
-}
-
-struct Peer {
-    addr: SocketAddrV4,
-    state: State,
-    connection: Connection,
-}
-
-impl Debug for Peer {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("peer")
-            .field(&self.addr)
-            .field(&self.state)
-            .finish()
-    }
-}
-
-struct Connection {
-    stream: BufWriter<TcpStream>,
-    buffer: BytesMut,
-}
-
-impl Connection {
-    fn new(s: TcpStream) -> Self {
-        Self {
-            stream: BufWriter::new(s),
-            buffer: BytesMut::with_capacity(1024),
-        }
-    }
-
-    async fn write_message(&mut self, m: Message) -> io::Result<()> {
-        match m {
-            Message::Ping => {
-                // self.stream.write_u8(b'>').await?;
-                self.stream.write_all("PING".as_bytes()).await?;
-            }
-            Message::Pong => {
-                // self.stream.write_u8(b'>').await?;
-                self.stream.write_all("PONG".as_bytes()).await?;
-            }
-        }
-        self.stream.write_all(b"\r\n").await?;
-        Ok(())
-    }
-
-    pub async fn read_message(&mut self) -> Option<Message> {
-        loop {
-            if let Some(message) = self.parse_message().await {
-                return Some(message);
-            }
-
-            let num_bytes = self.stream.read_buf(&mut self.buffer).await.unwrap();
-            if num_bytes == 0 {
-                if self.buffer.is_empty() {
-                    return None;
-                } else {
-                    // connection reset by peer
-                    return None;
-                }
-            }
-        }
-    }
-
-    async fn parse_message(&mut self) -> Option<Message> {
-        let mut buf = Cursor::new(&self.buffer[..]);
-        Message::check(&mut buf)
-    }
-}
-
-struct Node {
-    peers: Vec<Peer>,
-    address: Ipv4Addr,
-}
+type Error = Box<dyn std::error::Error + Send + Sync>;
 
 #[derive(Parser, Debug)]
 struct Args {
     bind_address: String,
 }
 
-#[derive(Debug)]
-enum Message {
-    Ping,
-    Pong,
-}
-
-#[derive(Debug)]
-enum Error {
-    Incomplete,
-}
-
-fn read_line<'a>(src: &mut Cursor<&'a [u8]>) -> Result<&'a [u8], Error> {
-    let start = src.position() as usize;
-    let end = src.get_ref().len() - 1;
-    for i in start..end {
-        if src.get_ref()[i] == b'\r' && src.get_ref()[i + 1] == b'\n' {
-            src.set_position((i + 2) as u64);
-            return Ok(&src.get_ref()[start..i]);
-        }
-    }
-
-    Err(Error::Incomplete)
-}
-
-impl Message {
-    pub fn check(src: &mut Cursor<&[u8]>) -> Option<Message> {
-        match src.get_u8() {
-            b'>' => {
-                let line = read_line(src).unwrap();
-                let message = String::from_utf8(line.to_vec()).unwrap();
-                match message.as_str() {
-                    "PING" => Some(Message::Ping),
-                    "PONG" => Some(Message::Pong),
-                    _ => None,
-                }
-            }
-            _ => None,
-        }
-    }
-}
-
-impl Peer {
-    fn new(addr: SocketAddrV4, stream: TcpStream) -> Self {
-        Self {
-            addr,
-            state: State::Offline,
-            connection: Connection::new(stream),
-        }
-    }
-
-    pub async fn ping(&mut self) -> io::Result<()> {
-        self.connection.write_message(Message::Ping).await
-    }
-}
-
 enum Command {
     AddPeer(Peer),
     MessageReceived(SocketAddrV4, Message),
     Quit,
+}
+
+async fn peer_handler(mut peer: Peer) -> anyhow::Result<()> {
+    loop {
+        let msg = peer.read().await.unwrap();
+        match msg {
+            Some(Message::Ping) => {
+                peer.pong().await?
+            },
+            Some(Message::Pong) => {
+                println!("HANDSHAKE COMPLETED!")
+            },
+            Some(_) => todo!(),
+            None => {}
+        }
+    }
 }
 
 async fn client(mut rx: mpsc::Receiver<Command>) {
@@ -168,12 +61,33 @@ async fn client(mut rx: mpsc::Receiver<Command>) {
                 match command {
                     Command::AddPeer(mut peer) => {
                         println!("found new peer: {peer:?}");
+
+                        // Peer handler
                         tokio::spawn(async move {
+                            peer.ping().await;
                             loop {
-                                peer.listen().await;
+                                println!("handler for peer {:?}...", peer);
+                                match peer.read().await {
+                                    Ok(Some(msg)) => match msg {
+                                        Message::Ping => {
+                                            peer.pong().await;
+                                        },
+                                        Message::Pong => {
+                                            println!("HANDSHAKE COMPLETED!");
+                                        },
+                                        Message::Info(m) => {
+                                            println!("received info message: {m}");
+                                        }
+                                    },
+                                    Ok(None) => {},
+                                    Err(e) => {
+                                        println!("e: {e:?}");
+                                        println!("Tearing down peer handler");
+                                        break
+                                    }
+                                }
                             }
                         });
-                        peer.ping().await;
                     },
                     Command::MessageReceived(from, msg) => {
                         println!("{from:?} sent {msg:?}");
