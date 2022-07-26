86af1f95aec722dd46e6c9ce31edd1a1329fc3c7
aaronwinter <erwan.ounn.84@gmail.com>
2022-07-26 18:59:01 +0300

    state: state machine scaffolding

--- src/state.rs
+++ src/state.rs
@@ -1,7 +1,34 @@
+use crate::message::Message;
+
+pub struct PeerStateMachine {
+    state: State,
+    log: Vec<Message>,
+}
+
 #[derive(Debug)]
 pub enum State {
     Offline,
     Syncing,
     Connected,
 }
+
+impl Default for State {
+    fn default() -> Self {
+        State::Offline
+    }
+}
+
+impl PeerStateMachine {
+    fn new() -> Self {
+        Self {
+            state: State::default(),
+            log: Vec::new(),
+        }
+    }
+
+    fn apply(&mut self, msg: Message) -> Result<(), crate::Error> {
+        unimplemented!()
+    }
+}
+
 
