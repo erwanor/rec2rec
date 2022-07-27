ad840c6400da28f48131e5f33ccdbb7c89b06ca5
aaronwinter <erwan.ounn.84@gmail.com>
2022-07-27 15:21:08 +0300

    feat: scaffolding `tower::Service` for `Peer`

--- src/peer.rs
+++ src/peer.rs
@@ -1,11 +1,15 @@
+use futures::Future;
 use tracing::info;
 use std::fmt::{Debug, Formatter};
 use std::fmt;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, ToSocketAddrs};
+use std::pin::Pin;
+use std::task::{Poll, Context};
 use tokio::net::TcpStream;
 use crate::connection::Connection;
 use crate::state::State;
 use tokio::io::BufWriter;
+use tower::Service;
 use tokio::sync::mpsc;
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
@@ -30,6 +34,22 @@ pub struct Peer {
     connection: Connection,
     clock: u32,
 }
+
+impl Service<Message> for Peer {
+    type Response = Message;
+    type Error = PeerError;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send + 'static>>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, request: Message) -> Self::Future {
+        unimplemented!()
+    }
+}
+
+
 
 impl Debug for Peer {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
@@ -60,21 +80,23 @@ impl Peer {
     }
 
     pub async fn heartbeat(&mut self) -> io::Result<()> {
-        self.connection.write_message(Message::Heartbeat(self.clock)).await
+        info!("HEARTBEAT({}) to {}", self.clock, self.addr);
+        let clock = self.clock;
+        self.clock += 1;
+        self.connection.write_message(Message::Heartbeat(clock)).await
     }
 
     pub async fn ping(&mut self) -> io::Result<()> {
-        info!("pinging!");
+        info!("PING to {}", self.addr);
         self.connection.write_message(Message::Ping).await
     }
 
     pub async fn pong(&mut self) -> io::Result<()> {
-        info!("ponging!");
+        info!("PONG to {}", self.addr);
         self.connection.write_message(Message::Pong).await
     }
 
     pub async fn read(&mut self) -> Result<Option<Message>, Error> {
-        info!("read_message:");
         self.connection.read_message().await
     }
 }
